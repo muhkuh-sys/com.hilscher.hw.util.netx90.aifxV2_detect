d4611a4d73a6ef67f74576c5353ea90374f335f5
Armin Ballmert <aballmert@hilscher.com>
2020-01-28 13:37:01 +0100

    [code] integrated basic snipped
    - compiler error due to invalid cast etc.

--- src/header.h
+++ src/header.h
@@ -22,6 +22,13 @@
 #ifndef __HEADER_H__
 #define __HEADER_H__
 
+// unlock register
+const unsigned long volatile *asic_ctrl_access_key = 0xff4012c0;
+// pull down enable (enable pulldown with bit 4)
+const unsigned char uc_pull_down_enable =  4;
+const unsigned long *pul_pad_ctrl_com_io1 = 0xff40101c;
+const unsigned long *pul_pad_ctrl_com_io2 = 0xff401020;
+const unsigned long *pul_pad_ctrl_mii0_txd1 = 0xff401054;
 
 typedef struct VERSION_HEADER_STRUCT
 {
@@ -30,8 +37,26 @@ typedef struct VERSION_HEADER_STRUCT
 	unsigned long ulVersionMicro;
 	const char    acVersionVcs[16];
 } VERSION_HEADER_T;
+// mask bits have to be 1, not checked for (assumed in reset state)
+const unsigned long *pul_clock_enable0_mask =  0xff40126c;
+// wm: set register and accordingly it's write mask to change the value
+const unsigned char bit_xc_misc = 8;
+const unsigned char bit_clock_xMAC0 = 4;
+const unsigned char bit_xc_misc_wm = 24;
+const unsigned char bit_clock_xMAC0_wm = 20;
+const unsigned long *pul_clock_enable0 = 0xff401268;
 
 extern const VERSION_HEADER_T tVersionHeader __attribute__ ((section (".header")));
 
+// pinns of register en_xmac_status_shared0 where to retrieve the status from
+const unsigned long *pul_xmac_status_shared0 = 0xff111440;
+typedef enum EN_XMAC_STATUS_SHARED_0{
+  pin_gpio0_in,
+  pin_gpio1_in,
+  pin_gpio2_in_phy_led0,
+  pin_gpio3_in_phy_led1,
+  pin_gpio4_in_phy_led2, // COM_IO1 (speed)
+  pin_gpio5_in_phy_led3, // COM_IO0 (duplex)
+}en_xmac_status_shared0;
 
 #endif  /* __HEADER_H__ */
--- src/main.c
+++ src/main.c
@@ -1,62 +1,91 @@
 #include "netx_io_areas.h"
 #include "rdy_run.h"
+#include "header.h"
+
 
 unsigned char getMmioValue(unsigned char mmioIndex)
 {
-	HOSTDEF(ptMmioCtrlArea);
+  HOSTDEF(ptMmioCtrlArea);
 
-	unsigned char ucRegisterIndex;
-	unsigned char ucIndexInRegister;
-	unsigned char ucBitResult;
-	unsigned long ulTemp;
+  unsigned char ucRegisterIndex;
+  unsigned char ucIndexInRegister;
+  unsigned char ucBitResult;
+  unsigned long ulTemp;
 
-	/* Extract Register Index 0-3*/
-	ucRegisterIndex = (unsigned char)((mmioIndex & 0xe0) >> 5);
+  /* Extract Register Index 0-3*/
+  ucRegisterIndex = (unsigned char) ((mmioIndex & 0xe0) >> 5);
 
-	/* Extract Index in register 0-31*/
-	ucIndexInRegister = (unsigned char)(mmioIndex & 0x1f);
+  /* Extract Index in register 0-31*/
+  ucIndexInRegister = (unsigned char) (mmioIndex & 0x1f);
 
-	/* Look in extracted register after Index stored in mmioIndex*/
+  /* Look in extracted register after Index stored in mmioIndex*/
 #if ASIC_TYP==ASIC_TYP_NETX90
-	ulTemp = ptMmioCtrlArea->ulMmio_in_line_status0;
+  ulTemp = ptMmioCtrlArea->ulMmio_in_line_status0;
 #elif ASIC_TYP==ASIC_TYP_NETX4000
-	ulTemp = ptMmioCtrlArea->aulMmio_in_line_status[ucRegisterIndex];
+  ulTemp = ptMmioCtrlArea->aulMmio_in_line_status[ucRegisterIndex];
 #endif
-	ucBitResult = (ulTemp >> ucIndexInRegister) & 0x1;
+  ucBitResult = (ulTemp >> ucIndexInRegister) & 0x1;
 
-	return ucBitResult;
+  return ucBitResult;
 }
 
 void start(unsigned char *pucResult, unsigned long ulIndexRotaryMmios);
 
+/**
+ * @brief The next command may change a protected register
+ * @details Rewrite the same value of the register inti itselve. this tells
+ * the controller to allow a change of the configuration section.
+ */
+inline void activateLock( void ){
+  *asic_ctrl_access_key = *asic_ctrl_access_key;
+}
+
 void __attribute__ ((section (".init_code"))) start(unsigned char *pucResult, unsigned long ulIndexRotaryMmios)
 {
-	int i;
-	int iEnd;
-	unsigned char ucIdx;
-	unsigned char ucBitValue;
-	unsigned char ucResult;
+  unsigned char ucResult;
+  ucResult = 0;
 
-	ucResult = 0;
+  #if ASIC_TYP==ASIC_TYP_NETX90
 
-#if ASIC_TYP==ASIC_TYP_NETX90
-	iEnd = 1;
-#elif ASIC_TYP==ASIC_TYP_NETX4000
-	iEnd = 4;
-#endif
+  #elif ASIC_TYP==ASIC_TYP_NETX4000
 
-	for(i=0; i<iEnd; i++) {
-		/* Extract MMIO index of R1 */
-		ucIdx = (unsigned char)((ulIndexRotaryMmios >> (24-(8*i) )) & 0xff);
+  #endif
 
-		/* Check if MMIO is used */
-		if (ucIdx != 0xff)
-		{
-			ucBitValue = getMmioValue(ucIdx);
-			ucResult = (ucResult << 1) | ucBitValue;
-		}
-	}
+  // enable pull downs of sense pins
+  activateLock();
+  *pul_pad_ctrl_com_io1   |= 1 << uc_pull_down_enable;
+  activateLock();
+  *pul_pad_ctrl_com_io2   |= 1 << uc_pull_down_enable;
+  activateLock();
+  *pul_pad_ctrl_mii0_txd1 |= 1 << uc_pull_down_enable;
 
-	/* Write the value to the pointer. */
-	*pucResult = ucResult;
+  // switch on group clock and enable pass clock to unit
+  unsigned long enableValueClocks = ( bit_xc_misc | bit_xc_misc_wm ) | ( bit_clock_xMAC0 | bit_clock_xMAC0_wm );
+  // just set the write mask, all other values are 0 so bothe bits will flip back to 0
+  unsigned long disableValueClocks = bit_xc_misc_wm | bit_clock_xMAC0_wm;
+  activateLock();
+  *pul_clock_enable0 = enableValueClocks;
+
+  ucResult = pul_xmac_status_shared0;
+  // read IO-Pins, to select the type of the bus peripheral
+
+  activateLock();
+  *pul_clock_enable0 = disableValueClocks;
+  volatile int a = 0;
+  if( ucResult && 1 << pin_gpio0_in){
+    a += 1;
+  }
+  if( usResult && 1 << pin_gpio4_in_phy_led2){
+    a += 2;
+  }
+  if( usResult && 1 << pin_gpio5_in_phy_led3){
+    a += 4;
+  }
+
+
+
+
+
+  /* Write the value to the pointer. */
+  *pucResult = ucResult;
 }
