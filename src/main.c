b9f5d3275a824745937de9478b769d4306048026
Armin Ballmert <aballmert@hilscher.com>
2020-01-28 17:24:20 +0100

    [code] removed unnecessary defines and corrected errors

--- src/main.c
+++ src/main.c
@@ -3,31 +3,7 @@
 #include "header.h"
 
 
-unsigned char getMmioValue(unsigned char mmioIndex)
-{
-  HOSTDEF(ptMmioCtrlArea);
 
-  unsigned char ucRegisterIndex;
-  unsigned char ucIndexInRegister;
-  unsigned char ucBitResult;
-  unsigned long ulTemp;
-
-  /* Extract Register Index 0-3*/
-  ucRegisterIndex = (unsigned char) ((mmioIndex & 0xe0) >> 5);
-
-  /* Extract Index in register 0-31*/
-  ucIndexInRegister = (unsigned char) (mmioIndex & 0x1f);
-
-  /* Look in extracted register after Index stored in mmioIndex*/
-#if ASIC_TYP==ASIC_TYP_NETX90
-  ulTemp = ptMmioCtrlArea->ulMmio_in_line_status0;
-#elif ASIC_TYP==ASIC_TYP_NETX4000
-  ulTemp = ptMmioCtrlArea->aulMmio_in_line_status[ucRegisterIndex];
-#endif
-  ucBitResult = (ulTemp >> ucIndexInRegister) & 0x1;
-
-  return ucBitResult;
-}
 
 void start(unsigned char *pucResult, unsigned long ulIndexRotaryMmios);
 
@@ -37,7 +13,9 @@ void start(unsigned char *pucResult, unsigned long ulIndexRotaryMmios);
  * the controller to allow a change of the configuration section.
  */
 inline void activateLock( void ){
-  *asic_ctrl_access_key = *asic_ctrl_access_key;
+  //*asic_ctrl_access_key = *asic_ctrl_access_key;
+  NX90_DEF_ptAsicCtrlArea
+  ptAsicCtrlArea->ulAsic_ctrl_access_key = ptAsicCtrlArea->ulAsic_ctrl_access_key;
 }
 
 void __attribute__ ((section (".init_code"))) start(unsigned char *pucResult, unsigned long ulIndexRotaryMmios)
@@ -52,39 +30,48 @@ void __attribute__ ((section (".init_code"))) start(unsigned char *pucResult, un
   #endif
 
   // enable pull downs of sense pins
+  NX90_DEF_ptAsicCtrlArea // variablen deklaration
+  NX90_DEF_ptPadCtrlArea
+  NX90_DEF_ptXc0Xmac0RegsArea
+
+  
+
+
   activateLock();
-  *pul_pad_ctrl_com_io1   |= 1 << uc_pull_down_enable;
+  ptPadCtrlArea->aulPad_ctrl_com_io[1] = MSK_NX90_pad_ctrl_com_io1_pe;
   activateLock();
-  *pul_pad_ctrl_com_io2   |= 1 << uc_pull_down_enable;
+  ptPadCtrlArea->aulPad_ctrl_com_io[2] = MSK_NX90_pad_ctrl_com_io2_pe;
   activateLock();
-  *pul_pad_ctrl_mii0_txd1 |= 1 << uc_pull_down_enable;
+  ptPadCtrlArea->aulPad_ctrl_mii0_txd[1] = MSK_NX90_pad_ctrl_mii0_rxd1_pe;
 
   // switch on group clock and enable pass clock to unit
-  unsigned long enableValueClocks = ( bit_xc_misc | bit_xc_misc_wm ) | ( bit_clock_xMAC0 | bit_clock_xMAC0_wm );
-  // just set the write mask, all other values are 0 so bothe bits will flip back to 0
-  unsigned long disableValueClocks = bit_xc_misc_wm | bit_clock_xMAC0_wm;
-  activateLock();
-  *pul_clock_enable0 = enableValueClocks;
+  
+  unsigned long enableValueClocks = ( MSK_NX90_clock_enable0_xc_misc | MSK_NX90_clock_enable0_xc_misc_wm ) | ( MSK_NX90_clock_enable0_xmac0 | MSK_NX90_clock_enable0_xmac0_wm );
+  unsigned long disableValueClocks = MSK_NX90_clock_enable0_xc_misc_wm | MSK_NX90_clock_enable0_xmac0_wm;
 
-  ucResult = pul_xmac_status_shared0;
+  activateLock();
+  ptAsicCtrlArea->asClock_enable[0].ulEnable = enableValueClocks;
+  // just set the write mask, all other values are 0 so bothe bits will flip back to 0
+
+
+  // *pul_clock_enable0 = enableValueClocks;
+
+  unsigned long ulResult = ptXc0Xmac0RegsArea->ulXmac_status_shared0;
+
   // read IO-Pins, to select the type of the bus peripheral
 
   activateLock();
-  *pul_clock_enable0 = disableValueClocks;
-  volatile int a = 0;
-  if( ucResult && 1 << pin_gpio0_in){
-    a += 1;
+  ptAsicCtrlArea->asClock_enable[0].ulEnable = disableValueClocks;
+
+  if( ulResult && 1 << pin_gpio0_in){
+    ucResult |= 1<<0;
   }
-  if( usResult && 1 << pin_gpio4_in_phy_led2){
-    a += 2;
+  if( ulResult && 1 << pin_gpio4_in_phy_led2){
+    ucResult |= (unsigned char) (1<<1 );
   }
-  if( usResult && 1 << pin_gpio5_in_phy_led3){
-    a += 4;
+  if( ulResult && 1 << pin_gpio5_in_phy_led3){
+    ucResult |= (unsigned char)1<<2 & 0xff;
   }
-
-
-
-
 
   /* Write the value to the pointer. */
   *pucResult = ucResult;
