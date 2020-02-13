80b689f51daa87a3f11082c55ed5129e4258e3b1
armin <aballmert@hilscher.com>
2020-02-13 16:13:23 +0100

    [script] renamed tcl-script

--- verification/test02/test.bat
+++ verification/test02/test.bat
@@ -2,10 +2,11 @@ echo off
 REM Start openOCD. The openocd-0.10.0-dev.exe is used to support the gets-user interaction
 set OPENOCD_BIN=openocd-0.10.0-dev.exe
 
-set OPENOCD_ATTACH_COM=netX90_test_aifxV2_detect_snippet.tcl
+set OPENOCD_ATTACH_COM=test02_netX90_test_aifxV2_detect_snippet.tcl
 
 
-%OPENOCD_BIN% -f %OPENOCD_ATTACH_COM%
+
+%OPENOCD_BIN% -f %OPENOCD_ATTACH_COM% -c shutdown
 set script_return=%errorlevel%
 echo "Script returned with %script_return%"
 exit /b %script_return%
